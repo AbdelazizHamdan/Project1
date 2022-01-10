54cb6d71d845c4e34cecabdf05e9a5f2f8f459fd
Abdelaziz Hamdan <abdelaziz@jawaker.com>
2022-01-10 14:47:36 +0200

    Add admin functions

---
+++ db/migrate/20220110120849_drop_password_digist_column.rb
@@ -0,0 +1,5 @@
+class DropPasswordDigistColumn < ActiveRecord::Migration[5.2]
+  def change
+    remove_column :users, :password_digist
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_10_050137) do
+ActiveRecord::Schema.define(version: 2022_01_10_120849) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -25,8 +25,8 @@ ActiveRecord::Schema.define(version: 2022_01_10_050137) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "password_digist"
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
 end
